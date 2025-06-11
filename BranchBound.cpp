7d53cf871852e2a077f4a6e3e62005a12ff82ccb
Nele <2020606313@qq.com>
2025-06-11 22:15:22 +0800

    1

--- BranchBound.cpp
+++ BranchBound.cpp
@@ -7,6 +7,8 @@
 #include <unordered_set>
 #include <vector>
 #include <string>
+#include <queue>
+
 
 //=========================生成初始解（无任何调试输出）=========================
 std::pair<BatchMap, double> generateInitialSolution(
@@ -292,6 +294,12 @@ double compute_unassigned_lower_bound(
     return node.total_tardiness + unassigned_tardiness;
 }
 
+struct CompareNode {
+    bool operator()(const Node& a, const Node& b) const {
+        return a.LB > b.LB; // 小的 LB 优先
+    }
+};
+
 
 
 
@@ -337,8 +345,8 @@ std::pair<Node, Stats> branch_and_cut(
     Node root({}, 0.0, "Root", 0.0, 0.0,0);  // 修复初始化
     root.LB = compute_unassigned_lower_bound(root, parts, D, ST, VT, UT, h, v);
 
-    std::deque<Node> stack;
-    stack.push_back(root);
+    std::priority_queue<Node, std::vector<Node>, CompareNode> stack;
+    stack.push(root);
 
     auto t0 = std::chrono::steady_clock::now();
     if (UB > 0 && UB < std::numeric_limits<double>::infinity()) {
@@ -358,12 +366,7 @@ std::pair<Node, Stats> branch_and_cut(
 
         if (!stack.empty()) {
             double timestamp = std::chrono::duration<double>(std::chrono::steady_clock::now() - t0).count();
-            double min_LB = std::numeric_limits<double>::infinity();
-            for (const Node& nd : stack) {
-                if (nd.LB < min_LB) {
-                    min_LB = nd.LB;
-                }
-            }
+            double min_LB = stack.top().LB;
 
             // 判断是否超过 UB
             if (min_LB >= UB) {
@@ -381,8 +384,8 @@ std::pair<Node, Stats> branch_and_cut(
         }
 
 
-        Node cur = stack.back();
-        stack.pop_back();
+        Node cur = stack.top();
+        stack.pop();
         ++stats.total_nodes;
 
         // 初步不可行剪枝
@@ -463,7 +466,7 @@ std::pair<Node, Stats> branch_and_cut(
 
             // 4. 剪枝判断
             if (child.LB < UB) {
-                stack.push_back(child);
+                stack.push(child);
             }
             else {
                 ++stats.LB_pruned_nodes;
